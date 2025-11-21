f0fb9c9e952d47757154c65b6ed5e9e872f6ab38
0x-DevSec <h.annane.dev@protonmail.com>
2025-11-21 12:15:42 +0100

    challenge 2: On click, change this divs background color

--- 00_Day1/script.js
+++ 00_Day1/script.js
@@ -3,3 +3,9 @@ window.addEventListener('load',function(){
     const div1 = document.getElementById('challenge-1')
     div1.style.backgroundColor = "red"
 })
+
+// challenge 2: On click, change this div’s background color
+window.addEventListener('click',function(){
+    const div2 = document.getElementById('challenge-2')
+    div2.style.backgroundColor = "blue";
+})

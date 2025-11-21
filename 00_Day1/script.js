6d87ef96608d24bbcbe035cd6900bb06153a2c31
0x-DevSec <h.annane.dev@protonmail.com>
2025-11-21 22:10:20 +0100

    challenge 7 + 8 solution

--- 00_Day1/script.js
+++ 00_Day1/script.js
@@ -44,6 +44,69 @@ input.addEventListener('input',function(){
     output.textContent = input.value;
 })
 
+// challenge7: Add a form; on submit, validate that input isn’t empty.
+
+const div7 = document.getElementById('challenge-7');
+const form = document.createElement('form');
+const myinput = document.createElement('input');
+const btn = document.createElement('button');
+
+
+myinput.type = 'text';
+myinput.placeholder = 'Type something...';
+btn.textContent = 'Submit';
+
+
+form.appendChild(myinput);
+form.appendChild(btn);
+div7.appendChild(form);
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    if (myinput.value.trim() === "") {
+        alert('Input can’t be empty');
+    } else {
+        alert('Form submitted successfully!');
+    }
+});
+
+// challenge8: On hover, increase this div’s size smoothly.
+
+const div8 = document.getElementById('challenge-8');
+
+div8.style.width = '90px';
+div8.style.height = '90px';
+div8.style.backgroundColor = "lightgray";
+div8.style.transition = "all 0.3s ease"; 
+
+div8.addEventListener('mouseenter', function () {
+    div8.style.width = "100px";
+    div8.style.height = "100px";
+    div8.style.transform = "scale(1.1)";
+});
+
+div8.addEventListener('mouseleave', function () {
+    div8.style.width = "90px";
+    div8.style.height = "90px";
+    div8.style.transform = "scale(1)";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

dd5cc11d0402eac3dcc0d4d20460dbf8fb6223e0
0x-DevSec <h.annane.dev@protonmail.com>
2025-11-21 12:08:31 +0100

    Challenge 1: Change background on page load

---
+++ 00_Day1/script.js
@@ -0,0 +1,5 @@
+// Challenge 1: Change background on page load
+window.addEventListener('load',function(){
+    const div1 = document.getElementById('challenge-1')
+    div1.style.backgroundColor = "red"
+})
